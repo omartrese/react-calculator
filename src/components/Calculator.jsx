30d19718f32cd4399bfa4599515c020821fb4afe
omartrese <omarlayachi13@gmail.com>
2024-03-19 18:46:55 +0100

    functionality added

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,43 +1,82 @@
 import React from 'react'
 import './Calculator.css'
+import { useRef } from 'react'
+import { useState } from 'react'
 
 function Calculator() {
+
+    const [display, setDisplay] = useState('0');
+    
+    const deleteButton = () => display != '0' ? setDisplay(display.slice(0, -1)) : setDisplay('0');
+    
+    const addChar = (event) => { 
+        const value = event.target.innerText;
+        
+        if (display === '0' || display === 'Syntax/Math error!') {
+            setDisplay(value === '()' ? '(' : value);
+            return;
+        }
+    
+        if (value !== '()') {
+            setDisplay(display + value);
+            return;
+        }
+    
+        const isOpenBracketNeeded = display.lastIndexOf('(') <= display.lastIndexOf(')');
+    
+        setDisplay(display + (isOpenBracketNeeded ? '(' : ')'));
+    }
+    
+    const clearDisplay = () => {
+        setDisplay('0');
+    }
+
+    const calcDisplay = () => {
+        try
+        {
+            setDisplay(eval(display));
+        } catch(error)
+        {
+            setDisplay('Syntax/Math error!');
+        }
+    }
+
     return (
         <section>
             <div id="calculator">
                 <div className='container'>
                     <div className="row" id='display'>
-                       <p>asdf</p>
+                       <p>{display}</p>
                     </div>
                     <div className="row">
-                        <button>Del</button>
-                        <button>C</button>
-                        <button>%</button>
-                        <button>/</button>
+                        <button onClick={deleteButton}>Del</button>
+                        <button onClick={clearDisplay}>C</button>
+                        <button onClick={addChar}>%</button>
+                        <button onClick={addChar}>/</button>
                     </div>
                     <div className="row">
-                        <button>7</button>
-                        <button>8</button>
-                        <button>9</button>
-                        <button>x</button>
+                        <button onClick={addChar}>7</button>
+                        <button onClick={addChar}>8</button>
+                        <button onClick={addChar}>9</button>
+                        <button onClick={addChar}>*</button>
                     </div>
                     <div className="row">
-                        <button>4</button>
-                        <button>5</button>
-                        <button>6</button>
-                        <button>-</button>
+                        <button onClick={addChar}>4</button>
+                        <button onClick={addChar}>5</button>
+                        <button onClick={addChar}>6</button>
+                        <button onClick={addChar}>-</button>
                     </div>
                     <div className="row">
-                        <button>1</button>
-                        <button>2</button>
-                        <button>3</button>
-                        <button>+</button>
+                        <button onClick={addChar}>1</button>
+                        <button onClick={addChar}>2</button>
+                        <button onClick={addChar}>3</button>
+                        <button onClick={addChar}>+</button>
                     </div>
                     <div className="row">
-                        <button>+/-</button>
-                        <button>0</button>
-                        <button>.</button>
-                        <button id='equal'>=</button>
+                        <button onClick={addChar}>()</button>
+                        <button onClick={addChar}>0</button>
+                        <button onClick={addChar}>.</button>
+                        <button id='equal' onClick={calcDisplay}>=</button>
                     </div>
                 </div>  
             </div>
