d2525d11b4e1186355ff603db17ed1e522d2c233
omartrese <omarlayachi13@gmail.com>
2024-03-20 14:59:46 +0100

    a little more security
    xd

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -2,45 +2,50 @@ import React from 'react'
 import './Calculator.css'
 import { useRef } from 'react'
 import { useState } from 'react'
+import { Parser } from 'expr-eval'
 
 function Calculator() {
 
+    
     const [display, setDisplay] = useState('0');
     
     const deleteButton = () => display != '0' ? setDisplay(display.slice(0, -1)) : setDisplay('0');
     
-    const addChar = (event) => { 
-        const value = event.target.innerText;
+    const addChar = (value) => { 
         
         if (display === '0' || display === 'Syntax/Math error!') {
             setDisplay(value === '()' ? '(' : value);
             return;
         }
-    
+        
         if (value !== '()') {
             setDisplay(display + value);
             return;
         }
-    
+        
         const isOpenBracketNeeded = display.lastIndexOf('(') <= display.lastIndexOf(')');
-    
+        
         setDisplay(display + (isOpenBracketNeeded ? '(' : ')'));
     }
     
     const clearDisplay = () => {
         setDisplay('0');
     }
-
+    
     const calcDisplay = () => {
         try
         {
-            setDisplay(eval(display));
+            const parser = new Parser();
+            const expr = parser.parse(display);
+            console.log(expr.evaluate());
+            setDisplay(expr.evaluate());
         } catch(error)
         {
             setDisplay('Syntax/Math error!');
+            console.log(error);
         }
     }
-
+    
     return (
         <section>
             <div id="calculator">
@@ -49,34 +54,34 @@ function Calculator() {
                        <p>{display}</p>
                     </div>
                     <div className="row">
-                        <button onClick={deleteButton}>Del</button>
-                        <button onClick={clearDisplay}>C</button>
-                        <button onClick={addChar}>%</button>
-                        <button onClick={addChar}>/</button>
+                        <button onClick={() => deleteButton()}>Del</button>
+                        <button onClick={() => clearDisplay()}>C</button>
+                        <button onClick={() => addChar('%')}>%</button>
+                        <button onClick={() => addChar('/')}>/</button>
                     </div>
                     <div className="row">
-                        <button onClick={addChar}>7</button>
-                        <button onClick={addChar}>8</button>
-                        <button onClick={addChar}>9</button>
-                        <button onClick={addChar}>*</button>
+                        <button onClick={() => addChar('7')}>7</button>
+                        <button onClick={() => addChar('8')}>8</button>
+                        <button onClick={() => addChar('9')}>9</button>
+                        <button onClick={() => addChar('*')}>*</button>
                     </div>
                     <div className="row">
-                        <button onClick={addChar}>4</button>
-                        <button onClick={addChar}>5</button>
-                        <button onClick={addChar}>6</button>
-                        <button onClick={addChar}>-</button>
+                        <button onClick={() => addChar('4')}>4</button>
+                        <button onClick={() => addChar('5')}>5</button>
+                        <button onClick={() => addChar('6')}>6</button>
+                        <button onClick={() => addChar('-')}>-</button>
                     </div>
                     <div className="row">
-                        <button onClick={addChar}>1</button>
-                        <button onClick={addChar}>2</button>
-                        <button onClick={addChar}>3</button>
-                        <button onClick={addChar}>+</button>
+                        <button onClick={() => addChar('1')}>1</button>
+                        <button onClick={() => addChar('2')}>2</button>
+                        <button onClick={() => addChar('3')}>3</button>
+                        <button onClick={() => addChar('+')}>+</button>
                     </div>
                     <div className="row">
-                        <button onClick={addChar}>()</button>
-                        <button onClick={addChar}>0</button>
-                        <button onClick={addChar}>.</button>
-                        <button id='equal' onClick={calcDisplay}>=</button>
+                        <button onClick={() => addChar('()')}>()</button>
+                        <button onClick={() => addChar('0')}>0</button>
+                        <button onClick={() => addChar('.')}>.</button>
+                        <button id='equal' onClick={() => calcDisplay()}>=</button>
                     </div>
                 </div>  
             </div>
